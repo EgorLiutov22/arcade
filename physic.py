c80c6caa00a2c0c14ce6ac7c6893a5ad622f0305
wayne <eliutov22@gmail.com>
2024-07-03 20:30:18 +0500

    lesson physics 1

--- physic.py
+++ physic.py
@@ -30,6 +30,17 @@ PLAYER_MASS = 2.0
 PLAYER_MAX_HORIZONTAL_SPEED = 200
 PLAYER_MAX_VERTICAL_SPEED = 1600
 
+PLAYER_MOVE_FORCE_ON_GROUND = 8000
+
+PLAYER_JUMP_IMPULSE = 1800
+
+DEAD_ZONE = 0.1
+
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
+DISTANCE_TO_CHANGE_TEXTURE = 20
+
 
 
 class GameWindow(arcade.Window):
@@ -54,6 +65,7 @@ class GameWindow(arcade.Window):
         self.physics_engine = arcade.PymunkPhysicsEngine(damping=damping,
                                                          gravity=gravity)
 
+
     def setup(self):
         self.player_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
@@ -68,14 +80,45 @@ class GameWindow(arcade.Window):
         self.player_sprite.center_x = SPRITE_SIZE + SPRITE_SIZE / 2
         self.player_sprite.center_y = SPRITE_SIZE + SPRITE_SIZE / 2
         self.player_list.append(self.player_sprite)
+        self.physics_engine.add_sprite(self.player_sprite,
+                                       friction=PLAYER_FRICTION,
+                                       mass=PLAYER_MASS,
+                                       moment=arcade.PymunkPhysicsEngine.MOMENT_INF,
+                                       collision_type="player",
+                                       max_horizontal_velocity=PLAYER_MAX_HORIZONTAL_SPEED,
+                                       max_vertical_velocity=PLAYER_MAX_VERTICAL_SPEED)
+        self.physics_engine.add_sprite_list(self.wall_list,
+                                            friction=WALL_FRICTION,
+                                            collision_type="wall",
+                                            body_type=arcade.PymunkPhysicsEngine.STATIC)
 
     def on_key_press(self, key, modifiers):
-        pass
+        if key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+        elif key == arcade.key.UP:
+            if self.physics_engine.is_on_ground(self.player_sprite):
+                impulse = (0, PLAYER_JUMP_IMPULSE)
+                self.physics_engine.apply_impulse(self.player_sprite, impulse)
 
     def on_key_release(self, key, modifiers):
-        pass
+        if key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
 
     def on_update(self, delta_time):
+        if self.left_pressed and not self.right_pressed:
+            force = (-PLAYER_MOVE_FORCE_ON_GROUND, 0)
+            self.physics_engine.apply_force(self.player_sprite, force)
+            self.physics_engine.set_friction(self.player_sprite, 0)
+        elif self.right_pressed and not self.left_pressed:
+            force = (PLAYER_MOVE_FORCE_ON_GROUND, 0)
+            self.physics_engine.apply_force(self.player_sprite, force)
+            self.physics_engine.set_friction(self.player_sprite, 0)
+        else:
+            self.physics_engine.set_friction(self.player_sprite, 1.0)
         self.physics_engine.step()
 
     def on_draw(self):
@@ -84,6 +127,63 @@ class GameWindow(arcade.Window):
         self.item_list.draw()
         self.bullet_list.draw()
         self.player_list.draw()
+
+
+class PlayerSprite(arcade.Sprite):
+   def __init__(self):
+       super().__init__()
+
+       self.scale = SPRITE_SCALING_PLAYER
+
+       main_path = ":resources:images/animated_characters/female_person/femalePerson"
+
+       self.idle_texture_pair = arcade.load_texture_pair(f"{main_path}_idle.png")
+       self.jump_texture_pair = arcade.load_texture_pair(f"{main_path}_jump.png")
+       self.fall_texture_pair = arcade.load_texture_pair(f"{main_path}_fall.png")
+
+       self.walk_textures = []
+       for i in range(8):
+           texture = arcade.load_texture_pair(f"{main_path}_walk{i}.png")
+           self.walk_textures.append(texture)
+
+       self.texture = self.idle_texture_pair[0]
+
+       self.hit_box = self.texture.hit_box_points
+       self.character_face_direction = RIGHT_FACING
+       self.cur_texture = 0
+       self.x_odometer = 0
+
+   def pymunk_moved(self, physics_engine, dx, dy, d_angle):
+       if dx < -DEAD_ZONE and self.character_face_direction == RIGHT_FACING:
+           self.character_face_direction = LEFT_FACING
+       elif dx > DEAD_ZONE and self.character_face_direction == LEFT_FACING:
+           self.character_face_direction = RIGHT_FACING
+
+       is_on_ground = physics_engine.is_on_ground(self)
+
+       self.x_odometer += dx
+
+       if not is_on_ground:
+           if dy > DEAD_ZONE:
+               self.texture = self.jump_texture_pair[self.character_face_direction]
+               return
+           elif dy < -DEAD_ZONE:
+               self.texture = self.fall_texture_pair[self.character_face_direction]
+               return
+
+       if abs(dx) <= DEAD_ZONE:
+           self.texture = self.idle_texture_pair[self.character_face_direction]
+           return
+
+       if abs(self.x_odometer) > DISTANCE_TO_CHANGE_TEXTURE:
+
+           self.x_odometer = 0
+
+           self.cur_texture += 1
+           if self.cur_texture > 7:
+               self.cur_texture = 0
+           self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
+
 
 
 def main():

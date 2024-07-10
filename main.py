b2b3a6d6647e65163452023cc241dfaaa41d76df
EgorLiutov22 <93601126+EgorLiutov22@users.noreply.github.com>
2024-07-10 20:42:05 +0500

    Add scene

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
 
 
 class Game(arcade.Window):
@@ -13,25 +14,50 @@ class Game(arcade.Window):
            title="Работа со спрайтами",
        )
        self.player_sprite = None
-       SPRITE_SCALING_COIN = 0.2
-       self.coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+       self.player_list = None
+       self.ground_list = None
+       self.box_list = None
+       self.scene = None
+       # SPRITE_SCALING_COIN = 0.1
+       # self.coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
 
 
    def setup(self):
+       self.scene = arcade.Scene()
+       self.scene.add_sprite_list("Player")
+       self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
        self.player_sprite.center_x = 64
        self.player_sprite.center_y = 128
-       self.coin.center_x = 640
-       self.coin.center_y = 120
+       self.scene.add_sprite("Player", self.player_sprite)
+
+       for x in range(0, 1250, 64):
+           ground = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+           ground.center_x = x
+           ground.center_y = 32
+           self.scene.add_sprite("Walls", ground)
+
+       coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+       for coordinate in coordinate_list:
+           # Add a crate on the ground
+           box = arcade.Sprite(
+               ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+           )
+           box.position = coordinate
+           self.scene.add_sprite("Walls", box)
+
+
 
    def on_draw(self):
-       self.clear()
-       self.player_sprite.draw()
-       self.coin.draw()
+      self.clear()
+      # self.coin.draw()
+      self.scene.draw()
 
    def update(self, delta_time: float):
-       pass
+      pass
 
 
 if __name__ == "__main__":

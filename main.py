fc95eda029889c911cc830fbd056c5f4ddedfe5e
wayne <eliutov22@gmail.com>
2024-07-17 21:06:10 +0500

    change color in pause

--- main.py
+++ main.py
@@ -111,6 +111,28 @@ class GameView(arcade.View):
     def get_player_sprite(self):
         return self.player_sprite
 
+class InstructionView(arcade.View):
+    def on_show_view(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, self.window.width, 0, self.window.height)
+        arcade.set_background_color(arcade.color.REDWOOD)
+    def on_draw(self):
+        """ Draw this view """
+        self.clear()
+        arcade.draw_text("Начать игру", self.window.width / 2, self.window.height / 2,
+                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Нажми что бы начать", self.window.width / 2, self.window.height / 2-75,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, start the game. """
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
 
 class GameOverView(arcade.View):
     def __init__(self):
@@ -178,9 +200,9 @@ def main():
     """ Main function """
 
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    start_view = GameView()
+    start_view = InstructionView()
     window.show_view(start_view)
-    start_view.setup()
+    # start_view.setup()
     arcade.run()
 
 
